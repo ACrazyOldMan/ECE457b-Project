5fc7d3a711d467b2eb8ce01a339018fcaaa455f3
Insu Lee <hiinsu@hotmail.com>
2013-03-28 23:34:30 -0400

    Updated segmentFeatures.m

--- src/segmentFeatures.m
+++ src/segmentFeatures.m
@@ -21,51 +21,34 @@ function featureList = segmentFeatures( bwImg , algorithm , varargin )
     %     fill: Finds connected feature pixels using paint fill and extracts them as segments.
     
     %% Code
-    % Insu's Personal Preference
-    % set(0,'DefaultFigureWindowStyle','docked');
-
-    % configurations
-    % TODO: configs should have default that is overridable 
-    % via optional input param
-    MARGIN = 2;
-    [ ImgWidth ImgHeight ] = size(bwImg);
-
-    if strcmp( algorithm , 'fill' )
-        bwImg = imfill(bwImg,'holes');
-    end
-    
-    % Get Containing box of each features in the src bw image
-    stats = regionprops(bwImg, 'BoundingBox');
-
-    % project segmentations
-    % figure();
-    % clf;hold on;
-    % imshow(~bwImg);
-
-    % init return var
     featureList = {};
     
-    for index=1:length(stats)
-        box = stats(index).BoundingBox;
-        % U: Upper Left
-        % L: Length
-        % R: row, C: col
-        UC = max(1, box(1) - MARGIN);
-        UR = max(1, box(2) - MARGIN);
-        LC = min(ImgWidth, box(3) + MARGIN * 2);
-        LR = min(ImgHeight, box(4) + MARGIN * 2);
-        % [UC UR LC LR]
-        if LC + LR > 5
-            % Draw cropping boxes
-            rectangle('Position', [UC UR LC LR]);
-            % Extract Feature
-            feature = imcrop(bwImg,[UC UR LC LR]); 
-            feature = ~imclearborder(feature);
-            featureList{index} = feature;
-            % Crop image and save
-            imwrite( feature, strcat( sprintf('%03d', index),'.png' ), 'png');
-        end
-    end
+    if strcmp( algorithm , 'fill' )
+        % bwImg = imfill(bwImg,'holes');
+    elseif strcmp( alogirthm, 'default')
+        % default
+        MARGIN = 2;
+        [ ImgWidth ImgHeight ] = size(bwImg);
+    
+        % Get Containing box of each features in the src bw image
+        stats = regionprops(bwImg, 'BoundingBox');
 
-    % hold off;
+        for index=1:length(stats)
+            box = stats(index).BoundingBox;
+            % U: Upper Left, L: Length
+            % R: row,        C: col
+            UC = max(1, box(1) - MARGIN);
+            UR = max(1, box(2) - MARGIN);
+            LC = min(ImgWidth, box(3) + MARGIN * 2);
+            LR = min(ImgHeight, box(4) + MARGIN * 2);
+            if LC + LR > 5 % ignore small features
+                % Extract Feature
+                feature = imcrop(bwImg,[UC UR LC LR]); 
+                feature = ~imclearborder(feature);
+                featureList{index} = feature;
+            end
+        end
+    else
+        % Reseved for error message
+    end
 end
